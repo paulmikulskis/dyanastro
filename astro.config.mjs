0a5f578999ac16ec2259f25025c141482118af01
Paul Mikulskis <mikulskisp@gmail.com>
2023-03-15 14:46:45 -0400

    latest packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
+  site: 'https://dyanabutler.com',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
